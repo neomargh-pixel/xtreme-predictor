1bf410f9e27fc063f1e79bfbe555ab8962591024
neomargh-pixel <neomargh@gmail.com>
2026-07-21 22:52:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const animales = [
-{nombre:"Caballo", frecuencia:18, atraso:9},
-{nombre:"Tigre", frecuencia:15, atraso:6},
-{nombre:"León", frecuencia:13, atraso:11},
-{nombre:"Gallo", frecuencia:12, atraso:8},
-{nombre:"Caimán", frecuencia:10, atraso:14}
+{nombre:"🐎 Caballo", frecuencia:18, atraso:9},
+{nombre:"🐅 Tigre", frecuencia:15, atraso:6},
+{nombre:"🦁 León", frecuencia:13, atraso:11},
+{nombre:"🐓 Gallo", frecuencia:12, atraso:8},
+{nombre:"🐊 Caimán", frecuencia:10, atraso:14}
 ];
 
-function calcularPuntaje(animal){
-    return (animal.frecuencia * 3) + animal.atraso;
+function calcularPuntaje(a){
+    return (a.frecuencia*3)+a.atraso;
 }
 
-animales.forEach(a=>{
-    a.puntaje = calcularPuntaje(a);
-});
+function actualizarRanking(){
 
-animales.sort((a,b)=>b.puntaje-a.puntaje);
+    animales.forEach(a=>{
+        a.puntaje=calcularPuntaje(a);
+    });
 
-console.log(animales);
+    animales.sort((a,b)=>b.puntaje-a.puntaje);
+
+    const filas=document.querySelectorAll("table tr");
+
+    for(let i=1;i<filas.length && i<=animales.length;i++){
+
+        filas[i].cells[0].textContent=i;
+        filas[i].cells[1].textContent=animales[i-1].nombre;
+        filas[i].cells[2].textContent=animales[i-1].puntaje;
+
+    }
+
+}
+
+document.querySelector("button").addEventListener("click",actualizarRanking);
+
+actualizarRanking();
