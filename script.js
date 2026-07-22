fb59588e62919e16ed14a6184b890e7d736e752d
neomargh-pixel <neomargh@gmail.com>
2026-07-22 10:56:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,15 +14,3 @@ function agregarResultado() {
     celda2.innerHTML = animal;
     celda3.innerHTML = puntaje;
 }
-    let tabla = document.getElementById("tablaResultados");
-
-    let fila = tabla.insertRow();
-
-    let celda1 = fila.insertCell(0);
-    let celda2 = fila.insertCell(1);
-    let celda3 = fila.insertCell(2);
-
-    celda1.innerHTML = tabla.rows.length - 1;
-    celda2.innerHTML = "🐕 Perro";
-    celda3.innerHTML = "80";
-}
