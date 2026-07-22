d47a9f5bf9c23ab26ef061feeec04b4985288593
neomargh-pixel <neomargh@gmail.com>
2026-07-22 08:56:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ contador[item.animal]=(contador[item.animal]||0)+1;
 console.log(contador);
 
 });
+const tabla=document.querySelector("table");
+
+Object.entries(contador)
+.sort((a,b)=>b[1]-a[1])
+.slice(0,5)
+.forEach((item,i)=>{
+
+tabla.innerHTML+=`
+<tr>
+<td>${i+1}</td>
+<td>${item[0]}</td>
+<td>${item[1]}</td>
+</tr>
+`;
+
+});
