4fa52e62a05a423cdf27113cb17e2d9af6e0f276
neomargh-pixel <neomargh@gmail.com>
2026-07-22 13:38:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
-let animales = [
-    ["🐎 Caballo", 98],
-    ["🐅 Tigre", 94],
-    ["🦁 León", 91],
-    ["🐓 Gallo", 88],
-    ["🐊 Caimán", 85],
-    ["🐍 Serpiente", 82],
-    ["🐒 Mono", 80],
-    ["🦅 Águila", 78],
-    ["🐘 Elefante", 75],
-    ["🐂 Toro", 72]
-];
-
 function cargarAnimales() {
     let tabla = document.getElementById("tablaResultados");
 
-    for (let i = 0; i < animales.length; i++) {
+    // Borra las filas anteriores y deja solo el encabezado
+    tabla.innerHTML = `
+        <tr>
+            <th>#</th>
+            <th>Animal</th>
+            <th>Puntaje</th>
+        </tr>
+    `;
 
+    for (let i = 0; i < animales.length; i++) {
         let fila = tabla.insertRow();
 
-        let celda1 = fila.insertCell(0);
-        let celda2 = fila.insertCell(1);
-        let celda3 = fila.insertCell(2);
-
-        celda1.innerHTML = tabla.rows.length - 1;
-        celda2.innerHTML = animales[i][0];
-        celda3.innerHTML = animales[i][1];
+        fila.insertCell(0).innerHTML = i + 1;
+        fila.insertCell(1).innerHTML = animales[i][0];
+        fila.insertCell(2).innerHTML = animales[i][1];
     }
 }
+
+// Carga la tabla automáticamente al abrir la página
+window.onload = cargarAnimales;
