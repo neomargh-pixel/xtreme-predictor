72878e46cdd0557866601b6ebf4ee24c14b3463d
neomargh-pixel <neomargh@gmail.com>
2026-07-21 22:46:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("XTREME Predictor iniciado");
+const animales = [
+{nombre:"Caballo", frecuencia:18, atraso:9},
+{nombre:"Tigre", frecuencia:15, atraso:6},
+{nombre:"León", frecuencia:13, atraso:11},
+{nombre:"Gallo", frecuencia:12, atraso:8},
+{nombre:"Caimán", frecuencia:10, atraso:14}
+];
+
+function calcularPuntaje(animal){
+    return (animal.frecuencia * 3) + animal.atraso;
+}
+
+animales.forEach(a=>{
+    a.puntaje = calcularPuntaje(a);
 });
+
+animales.sort((a,b)=>b.puntaje-a.puntaje);
+
+console.log(animales);
