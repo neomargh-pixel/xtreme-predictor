a5863dece9f81385b55a84f1e22692e954434ea8
neomargh-pixel <neomargh@gmail.com>
2026-07-22 10:33:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-alert("JavaScript conectado");
-document.getElementById("miBoton").onclick = function() {
-    alert("El botón funciona");
-};
 function agregarResultado() {
-    alert("Botón funcionando");
+    let tabla = document.getElementById("tablaResultados");
+
+    let fila = tabla.insertRow();
+
+    let celda1 = fila.insertCell(0);
+    let celda2 = fila.insertCell(1);
+    let celda3 = fila.insertCell(2);
+
+    celda1.innerHTML = tabla.rows.length - 1;
+    celda2.innerHTML = "🐕 Perro";
+    celda3.innerHTML = "80";
 }
