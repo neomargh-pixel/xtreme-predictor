0051b827b5b79719b179047154bfc8fc1f1e6fe4
neomargh-pixel <neomargh@gmail.com>
2026-07-22 13:31:06 -0400

    Update animales.js

--- animales.js
+++ animales.js
@@ -1,1 +1,12 @@
-
+const animales = [
+  ["🐎 Caballo", 98],
+  ["🐅 Tigre", 94],
+  ["🦁 León", 91],
+  ["🐓 Gallo", 88],
+  ["🐊 Caimán", 85],
+  ["🐍 Serpiente", 82],
+  ["🐒 Mono", 80],
+  ["🦅 Águila", 78],
+  ["🐘 Elefante", 75],
+  ["🐂 Toro", 72]
+];
