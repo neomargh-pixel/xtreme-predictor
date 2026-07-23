05f8195fc3998722dfc5f5138261bfbc8481af2b
neomargh-pixel <neomargh@gmail.com>
2026-07-23 12:23:59 -0400

    Update algoritmo.js

--- algoritmo.js
+++ algoritmo.js
@@ -1,1 +1,15 @@
+function calcularRanking() {
 
+    for (let i = 0; i < animales.length; i++) {
+
+        let nombre = animales[i][0];
+
+        let veces = resultados.filter(r => r.animal === nombre).length;
+
+        animales[i][1] = veces;
+
+    }
+
+    animales.sort((a, b) => b[1] - a[1]);
+
+}
