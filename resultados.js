7711264f9de4435116e543254c66f3c4e44af94b
neomargh-pixel <neomargh@gmail.com>
2026-07-23 14:21:47 -0400

    Update resultados.js

--- resultados.js
+++ resultados.js
@@ -1,12 +1,1 @@
-var resultados = [
-    {animal:"🐎 Caballo", dias:5},
-    {animal:"🐅 Tigre", dias:2},
-    {animal:"🦁 León", dias:8},
-    {animal:"🐊 Caimán", dias:3},
-    {animal:"🐍 Serpiente", dias:10},
-    {animal:"🐒 Mono", dias:1},
-    {animal:"🐓 Gallo", dias:6},
-    {animal:"🦅 Águila", dias:4},
-    {animal:"🐘 Elefante", dias:12},
-    {animal:"🐂 Toro", dias:7}
-];
+const resultados = [];
