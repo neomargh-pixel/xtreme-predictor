56d90fc3b5efdd6ff7163c9e16d8d29f45e69121
neomargh-pixel <neomargh@gmail.com>
2026-07-24 01:32:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
-window.onload = function() {
+window.onload = function(){
 
-    let ranking = generarPronostico();
+    let datos = analizarResultados();
 
-    let tabla = document.getElementById("tablaResultados");
+    let top = document.getElementById("top10");
+    let pronostico = document.getElementById("pronostico");
+    let estadistica = document.getElementById("estadistica");
 
-    ranking.slice(0,10).forEach((item,index)=>{
 
-        let fila = tabla.insertRow();
+    top.innerHTML = "";
 
-        fila.insertCell(0).innerHTML = index + 1;
-        fila.insertCell(1).innerHTML = item.animal;
-        fila.insertCell(2).innerHTML = item.puntaje;
-        fila.insertCell(3).innerHTML = item.dias + " días sin salir";
+    datos.slice(0,10).forEach((item, index)=>{
+
+        top.innerHTML += `
+        <p>
+        ${index+1}. ${item.animal}
+        - Salió: ${item.salidas} veces
+        - Días sin salir: ${item.dias}
+        </p>
+        `;
 
     });
 
-    document.getElementById("destacado").innerHTML = ranking[0].animal;
 
-    let mayorAtraso = [...ranking].sort((a,b)=>b.dias-a.dias);
+    if(datos.length > 0){
 
-    document.getElementById("atrasado").innerHTML = mayorAtraso[0].animal;
+        pronostico.innerHTML =
+        "🔥 Mayor tendencia: " + datos[0].animal;
 
-    document.getElementById("confianza").innerHTML =
-Math.floor(Math.random() * 11) + 90 + "%";
-
-};
-function agregarResultado(){
-
-    let animal = document.getElementById("nuevoAnimal").value;
-
-    if(animal == ""){
-        alert("Escribe un animal");
-        return;
     }
 
-    resultados.push({
-        fecha: new Date().toISOString().slice(0,10),
-        animal: animal
-    });
 
-    alert("Resultado guardado");
+    estadistica.innerHTML =
+    "Animales analizados: " + datos.length;
 
-    location.reload();
-}
+};
