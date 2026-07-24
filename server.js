31555895f0925329954926c618dfc6bed52ccd6a
neomargh-pixel <neomargh@gmail.com>
2026-07-24 18:26:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require("express");
 
+const app = express();
+
+app.use(express.static("."));
+
+app.get("/api", (req, res) => {
+    res.json({
+        mensaje: "XTREME Predictor funcionando correctamente"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Servidor iniciado en el puerto " + PORT);
+});
