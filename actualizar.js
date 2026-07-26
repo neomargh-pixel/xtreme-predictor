e449411f6c56bbece7b326f98d8222c488c1acf9
neomargh-pixel <neomargh@gmail.com>
2026-07-26 09:48:54 -0400

    Update actualizar.js

--- actualizar.js
+++ actualizar.js
@@ -1,1 +1,12 @@
+async function actualizarResultados() {
 
+    const respuesta = await fetch("resultados.json");
+    const datos = await respuesta.json();
+
+    console.log("Resultados cargados:", datos);
+
+    return datos;
+
+}
+
+actualizarResultados();
