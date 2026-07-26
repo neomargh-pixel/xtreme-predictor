c8f442e193304e764ab5eefec955b7ae842ab6bf
neomargh-pixel <neomargh@gmail.com>
2026-07-26 15:00:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static("."));
+app.use(express.static(__dirname));
 
-app.get("/api", (req, res) => {
-    res.json({
-        mensaje: "XTREME Predictor funcionando correctamente"
-    });
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Servidor iniciado en el puerto " + PORT);
+    console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
