4db43d0e7f627c52b47bd15fb3e2988ac85587c5
neomargh-pixel <neomargh@gmail.com>
2026-07-27 22:52:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/resultados.json", (req, res) => {
+  res.sendFile(path.join(__dirname, "resultados.json"));
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor iniciado en el puerto ${PORT}`);
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
