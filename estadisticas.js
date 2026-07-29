4e0bbfef15ed2c09feccf1e1ab379af6d34015a3
neomargh-pixel <neomargh@gmail.com>
2026-07-29 12:33:37 -0400

    Update estadisticas.js

--- estadisticas.js
+++ estadisticas.js
@@ -1,1 +1,21 @@
+function estadisticasGenerales(){
 
+    let totalSorteos = resultados.length;
+
+    let totalAnimales = animales.length;
+
+    let masFrecuente = analizarResultados()[0];
+
+    return {
+
+        sorteos: totalSorteos,
+
+        animales: totalAnimales,
+
+        lider: masFrecuente.animal,
+
+        indice: masFrecuente.indice
+
+    };
+
+}
