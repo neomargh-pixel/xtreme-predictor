441359a3b35e3f10da8395f373c0b649ab9127e1
neomargh-pixel <neomargh@gmail.com>
2026-07-29 12:31:16 -0400

    Update probabilidad.js

--- probabilidad.js
+++ probabilidad.js
@@ -1,1 +1,28 @@
+function calcularProbabilidad(animal){
 
+    let historial = resultados.filter(r => r.animal === animal);
+
+    let salidas = historial.length;
+
+    let ultima = historial
+        .sort((a,b)=>new Date(b.fecha)-new Date(a.fecha))[0];
+
+    let dias = 30;
+
+    if(ultima){
+
+        dias = Math.floor(
+            (new Date() - new Date(ultima.fecha))
+            /(1000*60*60*24)
+        );
+
+    }
+
+    let probabilidad = (salidas*5)+(dias*2);
+
+    if(probabilidad>100)
+        probabilidad=100;
+
+    return probabilidad;
+
+}
