eefacaf95ed105a8850dbd11c81e90447959fa29
neomargh-pixel <neomargh@gmail.com>
2026-07-29 12:45:01 -0400

    Update motorXTEME.js

--- motorXTEME.js
+++ motorXTEME.js
@@ -1,1 +1,15 @@
+function generarPronosticoXTREME(){
 
+    let analisis = analizarResultados();
+
+    return analisis
+        .sort((a,b)=>b.indice-a.indice)
+        .slice(0,10);
+
+}
+
+function mejorAnimal(){
+
+    return generarPronosticoXTREME()[0];
+
+}
